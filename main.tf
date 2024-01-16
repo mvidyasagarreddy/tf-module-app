e1cf9fd778dfffb2639662ef88f1357ed5fbe365
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-01-16 22:05:44 +0530

    Roboshop Terraform

--- main.tf
+++ main.tf
@@ -1,8 +1,127 @@
-resource "null_resource" "test" {
-  triggers = {
-    xyz = timestamp()
+#resource "null_resource" "test" {
+#  triggers = {
+#    xyz = timestamp()
+#  }
+#  provisioner "local-exec" {
+#    command = "echo Hello World - Env - ${var.env}"
+#  }
+#}
+
+#=====================================================
+
+## Policy
+resource "aws_iam_policy" "policy" {
+  name        = "${var.component}-${var.env}-ssm-pm-policy"
+  path        = "/"
+  description = "${var.component}-${var.env}-ssm-pm-policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:984498312990:parameter/roboshop.${var.env}.${var.component}.*"
+      }
+    ]
+  })
+}
+
+
+# IAM Role
+resource "aws_iam_role" "role" {
+  name = "${var.component}-${var.env}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "ec2.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component}-${var.env}-ec2-role"
+  role = aws_iam_role.role.name
+}
+
+## Security Group
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  provisioner "local-exec" {
-    command = "echo Hello World - Env - ${var.env}"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+
+## Ec2
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.ami.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  tags = {
+    Name = "${var.component}-${var.env}"
+  }
+
+}
+
+## DNS Record
+resource "aws_route53_record" "dns" {
+  zone_id = "Z0858290PK34QY38ZGFN"
+  name    = "${var.component}-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.instance.private_ip]
+}
+
+## Null Resource - Ansible
+
+resource "null_resource" "ansible" {
+  depends_on = [aws_instance.instance,aws_route53_record.dns]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.public_ip
+    }
+
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/mvidyasagarreddy/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}"
+    ]
   }
 }
