8017d15c183831a87009a6a3fe2402eacf556a78
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-02-21 07:22:26 +0530

    Roboshop Terraform

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_policy" "policy" {
   path        = "/"
   description = "${var.component}-${var.env}-ssm-pm-policy"
 
-  # Terraform's "jsonencode" function converts a
+  # Terraform "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
   policy = jsonencode({
     "Version": "2012-10-17",
