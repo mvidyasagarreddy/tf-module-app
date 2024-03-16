6c83a1d54f542a0b60cdcc66fa26cb33231786b5
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-03-16 10:59:58 +0530

    Nginx Conf

--- main.tf
+++ main.tf
@@ -126,6 +126,7 @@ resource "null_resource" "ansible" {
 
     inline = [
       "sudo labauto ansible",
+      "sudo set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/mvidyasagarreddy/roboshop-ansible main.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
